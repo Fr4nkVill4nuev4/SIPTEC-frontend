f6d553d46aaaceed359ef3b35c748994a86010c1
Fr4nkVill4nuev4 <20250827@ricaldone.edu.sv>
2026-07-23 07:16:29 -0600

    Reemplazar var por const en app.config.js

--- src/config/app.config.js
+++ src/config/app.config.js
@@ -1,3 +1,3 @@
-﻿var SESSION_KEY = "siptec-session";
-var THEME_STORAGE_KEY = "siptec-theme";
-var API_FALLBACK_ORIGIN = "http://localhost:8080";
+const SESSION_KEY = "siptec-session";
+const THEME_STORAGE_KEY = "siptec-theme";
+const API_FALLBACK_ORIGIN = "http://localhost:8080";
